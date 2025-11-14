88d40888a62820546021d726630759368398b81d
Rahul kumar <rahulkmr33811@gmail.com>
2025-11-14 19:01:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
 
-package_name = 'udp_client'
+package_name = 'ros2_udp_client_for_robotic_adas_targets'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/udp_client.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='ROS2 UDP client node for sending structured commands over UDP.',
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='UDP client node for robotic ADAS targets.',
     license='MIT',
     entry_points={
         'console_scripts': [
-            'udp_client_node = udp_client.udp_client_node:main'
+            'udp_clientnode = ros2_udp_client_for_robotic_adas_targets.udp_clientnode:main',
         ],
     },
 )
